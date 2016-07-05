b0b46f3d6fa52863adad3bedc124ae07deeec07d
qyz55 <sjzwf501@126.com>
2016-07-05 20:21:02 +0800

    First Commit

---
+++ pro1/Assets/KinectView/Scripts/MoveOfMainCamera.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class test : MonoBehaviour {
+
+	// Use this for initialization
+    private int speed = 5;
+    public GameObject _pre_Cube;
+    private GameObject a;
+	void Start () {
+        a = GameObject.Instantiate(_pre_Cube);
+        a.transform.rotation = Quaternion.identity;
+        a.transform.position = new Vector3(0, 0, 0);
+	}
+	// Update is called once per frame
+	void Update () {
+        float x = Input.GetAxis("Horizontal") * Time.deltaTime * speed;
+        float z = Input.GetAxis("Vertical") * Time.deltaTime * speed;
+        transform.Translate(x, 0, z);
+        
+	}
+}
