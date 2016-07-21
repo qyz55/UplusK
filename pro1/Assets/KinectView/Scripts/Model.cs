14e00d40154a1d43188e0f020997b979f0c86b36
Carl Yu <yuaooauy@yeah.net>
2016-07-21 16:51:22 +0800

    delete trash

--- pro1/Assets/KinectView/Scripts/Model.cs
+++ pro1/Assets/KinectView/Scripts/Model.cs
@@ -1,7 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
-<<<<<<< HEAD
 public enum StateOfBlock:int
 {
     free       =0,//自由
@@ -9,16 +8,6 @@ public enum StateOfBlock:int
     /*aroundaxis,*/
     //轴对上但角度不对，有待商榷
     jointed    =2,//已被拼上，意味着不可动
-=======
-
-public enum StateOfBlock
-{
-    free,   //自由
-    caught, //被抓取
-    /*aroundaxis,*/
-    //轴对上但角度不对，有待商榷
-    jointed//已被拼上，意味着不可动
->>>>>>> 859783a0d9529979645e444a28443d6a21ab1961
 };
 public class Model
 {
